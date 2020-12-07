3d1281a512a0d4f801a419f67ffca13bf0c02bc9
KGJ7 <glectechs16@gmail.com>
2020-12-07 21:56:11 +0000

    add

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,7 @@ package com.company;
 
 import java.util.*;
 import java.io.*;
-
-
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -11,92 +10,134 @@ public class Main {
     public static int playerHp = 100;
     public static int enemyHp;
     public static int enemyType;
+    public static int tutorialProgress = 0;
     public static String enemyName;
     public static String enemyVariety;
     public static String weaponEquipped;
     public static String[] inventory = new String[5];
     public static Scanner scan = new Scanner(System.in);
     public static Random randint = new Random();
-    public static String[] lowDiffEnemyList ={
-                                            "wolf"+
-                                            "drunken man"+
-                                            "fox"};
-    public static String[] mediumDiffEnemyList = {
-                                               "gromp"+
-                                               "scuttle crab"+
-                                               "krug"};
-    public static String[] hardDiffEnemyList = {
-                                                "sentinel"+
-                                                "brambleback"+
-                                                "ironback turtle"};
-    public static String[][] weaponList = {
-                                          {"Pointy Stick" + "Wooden Sword" + "Iron Sword"},
-                                          {"8" + "12" + "18"}
+    public static String[] lowDiffEnemyList = {"wolf", "drunken man", "fox"};
+    public static String[] mediumDiffEnemyList = {"gromp", "scuttle crab", "krug"};
+    public static String[] hardDiffEnemyList = {"sentinel", "brambleback", "ironback turtle"};
+    public static String[][] weaponList = {{"Pointy Stick", "Wooden Sword", "Iron Sword"},
+            {"8", "12", "18"}
     };
     public static boolean playerAlive = true;
     public static boolean lightAttack;
+    public static boolean tutorialActive = true;
 
-    public static void runGame(){
+    public static void runGame() {
         openingStory();
-        while (playerAlive == true){
-            getUserInput();
-            if (playerHp < 1){
-                System.out.println("You are dead.");
+        tutorialActive = false;
+        storyPartOne();
+//        while (playerAlive) {
+//            getUserInput();
+            if (playerHp < 1) {
+                System.out.println("死\nYOU ARE DEAD.");
             }
         }
-    }
 
-
-    public static void openingStory(){
+//    }
+    public static void openingStory() {
         System.out.println("You wake up in a forest surrounded by bushes, completely empty-handed. \nThere is a pointy stick on the floor. \nYou pick it up.");
         addInventory(weaponList[0][0], 0);
         inventoryIntroduction();
     }
-
     public static void inventoryIntroduction() {
-        System.out.println("Now that you have a stick in your inventory, press the 'i' key to open your inventory!");
-        if (scan.next() != null){
-            combatIntroduction();
+            System.out.println("Now that you have a stick in your inventory, type 'i'.");
+            getUserInput();
+            if (tutorialProgress == 1){
+                try{
+                    TimeUnit.SECONDS.sleep(2);
+                    combatIntroduction();
+                }catch(Exception e){
+                    System.out.println("ERROR OCCURRED");
+                }
+            }
         }
+    public static void combatIntroduction() {
+        System.out.println("This stick has a use, that use being swinging it at your enemies! \nGet some practice in and use an attack against a tree! \nPress the 'q' key to perform a light attack!");
+        getUserInput();
+        if (tutorialProgress==2){
+            try {
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Great!\nNow try going for a heavy attack with the 'e' key!");
+                getUserInput();
+            }catch(Exception e){
+                System.out.println("ERROR OCCURRED");
+            }
+            if (tutorialProgress ==3){
+                tutorialActive = false;
+                return;
+            }
+        }
+    }
+    public static void currentWeaponEquipped() {
 
     }
-    public static void combatIntroduction(){
-        System.out.println("This stick has a use, that use being swinging it at your enemies! \nGet some practice in. \nPress the 'q' key to perform a light attack!");
-
+    public static void lightAttack() {
+        if (tutorialActive){
+            tutorialProgress += 1;
+        }
+        try {
+            lightAttack = true;
+            System.out.println("You quickly swing your weapon.");
+            TimeUnit.MILLISECONDS.sleep(1500);
+            System.out.println(getPlayerDamageDealt()+ " damage dealt!");
+        } catch (Exception e){
+            System.out.println("ERROR OCCURRED");
+        }
     }
-    public static void currentWeaponEquipped(){
+    public static void heavyAttack() {
+        if (tutorialActive){
+            tutorialProgress = tutorialProgress + 1;
+        }
+        try {
+            lightAttack = false;
+            System.out.println("You charge up an attack.");
+            TimeUnit.SECONDS.sleep(4);
+            System.out.println(getPlayerDamageDealt() + " damage dealt!");
+        } catch (Exception e){
+            System.out.println("ERROR OCCURRED");
 
-    }
-    public static void lightAttack(){
-        lightAttack = true;
-        System.out.println("You quickly swing your weapon.");
-        System.out.println(getPlayerDamageDealt());
-
-
+        }
     }
     public static void getUserInput() {
         char keyPressed = scan.nextLine().charAt(0);
-        switch (keyPressed){
-            case('i'):
+        switch (keyPressed) {
+            case ('i'):
                 displayInventory();
                 break;
-            case('q'):
+            case ('q'):
                 lightAttack();
+                break;
+            case ('e'):
+                heavyAttack();
+                break;
+            default:
+                System.out.println("Invalid Input!!");
                 break;
         }
     }
-
-    public static void storyPartOne(){
-        System.out.println();
+    public static void storyPartOne() {
+        while (true){
+            System.out.println("IT WORKS");
+            getUserInput();
+            break;
+        }
     }
-    public static void addInventory(String object, int place){
+    public static void addInventory(String object, int place) {
         inventory[place] = object;
     }
-    public static void displayInventory(){
-        for(int i = 0; i<5; i++){
-            if(inventory[i]==null){
+    public static void displayInventory() {
+        if (tutorialActive == true){
+            tutorialProgress += 1;
+        }
+        for (int i = 0; i < 5; i++) {
+            if (inventory[i] == null) {
                 System.out.println("(Empty)");
-            }else {
+            } else {
                 System.out.println(inventory[i]);
             }
         }
@@ -104,8 +145,8 @@ public class Main {
     public static int setEnemyHp(){
         int enemyType = randint.nextInt(3);
         if (enemyType == 1) {
-           enemyHp = 50;
-        }else if (enemyType == 2) {
+            enemyHp = 50;
+        } else if (enemyType == 2) {
             enemyHp = 100;
         } else {
             enemyHp = 150;
@@ -113,16 +154,16 @@ public class Main {
         return enemyType;
     }
     public static int enemyDamage() {
-        int multiplier = randint.nextInt(5) +1;
+        int multiplier = randint.nextInt(5) + 1;
         int baseEnemyDamage;
-        if(enemyHp<100 && enemyHp>75) {
-            baseEnemyDamage =4;
-        }else if(enemyHp<76 && enemyHp>25){
+        if (enemyHp < 100 && enemyHp > 75) {
+            baseEnemyDamage = 4;
+        } else if (enemyHp < 76 && enemyHp > 25) {
             baseEnemyDamage = 3;
-        }else{
+        } else {
             baseEnemyDamage = 4;
         }
-        int damageDealt = multiplier*baseEnemyDamage;
+        int damageDealt = multiplier * baseEnemyDamage;
         return damageDealt;
     }
     public static int playerDamageTaken(int a) {
@@ -134,14 +175,13 @@ public class Main {
             int playerDamageDealt = randint.nextInt(3) + 8;
             return playerDamageDealt;
         } else if (!lightAttack) {
-            int playerDamageDealt = randint.nextInt(3) + 12;
+            int playerDamageDealt = randint.nextInt(4) + 15;
             return playerDamageDealt;
         } else {
             System.out.println("ERROR");
         }
         return 0;
     }
-
     public static String getEnemyName() {
         if (enemyType == 1) {
             String enemyName = lowDiffEnemyList[randint.nextInt(3)];
@@ -153,31 +193,20 @@ public class Main {
             String enemyName = hardDiffEnemyList[randint.nextInt(3)];
         }
         return enemyName;
-
     }
-
-    public static void fight(){
+    public static void fight() {
         boolean fightingPhase = true;
         System.out.println("You have run into a " + getEnemyName() + "!");
+
     }
-
-
-    public static void main(String[] args) throws IOException {
-            runGame();
-
-
-//            setEnemyHp();
-//            for(int i = 0; i<10;i++){
-//            System.out.println(playerHp);
-//            int x = enemyDamage();
-//            System.out.println(x);
-//            playerDamageTaken(x);
-//            System.out.println(playerHp);
-//            System.out.println();
-
-
-
-
-        }
+    public static void main(String[] args) {
+        runGame();
     }
+}
+
+
+
+
+
+
 
